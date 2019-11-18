fd0e22c5c22acea11145eb5be74892ca0d3274b9
Vincent <vincent.l@wearephenix.com>
2019-11-18 10:54:35 +0100

    add spiderfier

--- ClusteredMapView.js
+++ ClusteredMapView.js
@@ -1,3 +1,5 @@
+import DB from 'realm-orm';
+
 'use-strict'
 
 // base libs
@@ -6,10 +8,12 @@ import React, { Component } from 'react'
 import {
   Platform,
   Dimensions,
-  LayoutAnimation
-} from 'react-native'
+  LayoutAnimation,
+  View,
+  Image,
+} from 'react-native';
 // map-related libs
-import MapView from 'react-native-maps'
+import MapView, { Marker } from 'react-native-maps';
 import SuperCluster from 'supercluster'
 import GeoViewport from '@mapbox/geo-viewport'
 // components / views
@@ -20,8 +24,15 @@ import {
   itemToGeoJSONFeature
 } from './util'
 import isEqual from 'lodash.isequal';
+import GeolocationHelper from '../../src/helpers/geolocation';
+import colors from '../../src/helpers/color';
 
 export default class ClusteredMapView extends Component {
+
+
+  ungroupLines = [];
+  groupCenter = [];
+  exploded = [];
 
   constructor(props) {
     super(props)
@@ -30,7 +41,7 @@ export default class ClusteredMapView extends Component {
       data: [], // helds renderable clusters and markers
       region: props.region || props.initialRegion, // helds current map region
     }
-
+    this.zoom = 1;
     this.isAndroid = Platform.OS === 'android'
     this.dimensions = [props.width, props.height]
 
@@ -52,6 +63,9 @@ export default class ClusteredMapView extends Component {
   componentWillReceiveProps(nextProps) {
     if (!isEqual(this.props.data, nextProps.data))
     {
+
+      ungroupLines = [];
+      groupCenter = [];
       this.clusterize(nextProps.data)
     }
     /*if (this.props.data !== nextProps.data)
@@ -70,6 +84,8 @@ export default class ClusteredMapView extends Component {
   getMapRef = () => this.mapview
 
   getClusteringEngine = () => this.index
+
+  getZoom = () => this.zoom
 
   clusterize = (dataset) => {
     this.index = new SuperCluster({ // eslint-disable-line new-cap
@@ -92,9 +108,12 @@ export default class ClusteredMapView extends Component {
   clustersChanged = (nextState) => this.state.data.length !== nextState.data.length
 
   onRegionChangeComplete = (region) => {
-    const data = this.getClusters(region)
+    var data = this.getClusters(region)
 
+
+    console.warn(data);
     if (!isEqual(data, this.state.data)) {
+      data = this.unexplode(data, region);
       return this.setState({ region, data }, () => {
         this.props.onRegionChangeComplete && this.props.onRegionChangeComplete(region, data)
       })
@@ -103,10 +122,149 @@ export default class ClusteredMapView extends Component {
     this.props.onRegionChangeComplete && this.props.onRegionChangeComplete(region, data)
   }
 
+  unexplode = (data, region) =>
+  {
+    if(this.exploded.length > 0)
+    {
+      let dataset = this.props.data.map((item) =>
+      {
+        if (!DB.db.isInTransaction)
+        {
+          DB.db.beginTransaction();
+        }
+        for (var i = 0; i < this.exploded.length; i++)
+        {
+          let explodedItem = this.exploded[i];
+
+          if (explodedItem.id === item.id)
+          {
+            item.latitude = explodedItem.latitude;
+            item.longitude = explodedItem.longitude;
+
+          }
+        }
+
+        if (DB.db.isInTransaction)
+        {
+          DB.db.commitTransaction();
+        }
+
+        return item;
+      });
+
+      this.ungroupLines = [];
+      this.groupCenter = [];
+
+      this.index = new SuperCluster({ // eslint-disable-line new-cap
+        extent: this.props.extent,
+        minZoom: this.props.minZoom,
+        maxZoom: this.props.maxZoom,
+        radius: this.props.radius || (this.dimensions[0] * .045), // 4.5% of screen width
+      })
+
+      // get formatted GeoPoints for cluster
+      const rawData = dataset.map(itemToGeoJSONFeature)
+
+      // load geopoints into SuperCluster
+      this.index.load(rawData)
+
+      return this.getClusters(region);
+    }
+
+    return data;
+  }
+
+  explode = (children) =>
+  {
+    let dataset = this.props.data;
+    let newArray = []
+    dataset = dataset.map((item) =>
+    {
+      if(!DB.db.isInTransaction)
+      {
+        DB.db.beginTransaction();
+      }
+      //console.warn("check", item);
+      const angle = 360 / children.length;
+      for(var i = 0; i < children.length; i++)
+      {
+        let childrenItem = children[i].properties.item;
+        //console.warn("childrenItem", childrenItem);
+        if (childrenItem.id === item.id)
+        {
+          console.warn("update", childrenItem.id);
+          /* if (i % 2 === 0)
+		   {*/
+          this.exploded.push({
+            id: childrenItem.id,
+            latitude: childrenItem.latitude,
+            longitude: childrenItem.longitude,
+          })
+          let newLocation = this._getDestinationFromDistance(Number(childrenItem.latitude), Number(childrenItem.longitude), 5, angle * i - 90);
+          //item.latitude  = '' + (Number(childrenItem.latitude) + ((0.1) / 2000));
+          //item.longitude = '' + (Number(childrenItem.longitude) + ((0.1) / 2000));
+          newArray.push({
+            id: childrenItem.id,
+            ...newLocation,
+          });
+          item.latitude = '' + newLocation.latitude;
+          item.longitude = '' + newLocation.longitude;
+          /* }
+		   else
+		   {
+			 item.latitude  = '' + (Number(childrenItem.latitude) - ((0.1) / 2000));
+			 item.longitude = '' + (Number(childrenItem.longitude) - ((0.1) / 2000));
+		   }*/
+
+        }
+      }
+
+      if(DB.db.isInTransaction)
+      {
+        DB.db.commitTransaction();
+      }
+
+      return item;
+    });
+
+
+    let contentLocation = newArray;
+
+    let centerLocation = {
+      latitude: parseFloat(children[0].geometry.coordinates[1]),
+      longitude: parseFloat(children[0].geometry.coordinates[0]),
+    };
+    this.groupCenter.push(centerLocation);
+    contentLocation.push(centerLocation);
+    contentLocation.map((location) =>
+    {
+      this.ungroupLines.push([centerLocation, location]);
+    });
+
+    this.index = new SuperCluster({ // eslint-disable-line new-cap
+      extent: this.props.extent,
+      minZoom: this.props.minZoom,
+      maxZoom: this.props.maxZoom,
+      radius: this.props.radius || (this.dimensions[0] * .045), // 4.5% of screen width
+    })
+
+    // get formatted GeoPoints for cluster
+    const rawData = dataset.map(itemToGeoJSONFeature)
+
+    // load geopoints into SuperCluster
+    this.index.load(rawData)
+
+    const data = this.getClusters(this.state.region);
+
+    console.warn(data);
+    this.setState({ data });
+
+  }
+
   getClusters = (region) => {
     const bbox = regionToBoundingBox(region),
-          viewport = (region.longitudeDelta) >= 40 ? { zoom: this.props.minZoom } : GeoViewport.viewport(bbox, this.dimensions)
-
+        viewport = (region.longitudeDelta) >= 40 ? { zoom: this.props.minZoom } : GeoViewport.viewport(bbox, this.dimensions)
+    this.zoom = viewport.zoom;
     return this.index.getClusters(bbox, viewport.zoom)
   }
 
@@ -115,12 +273,48 @@ export default class ClusteredMapView extends Component {
     this.props.onRegionChange && this.props.onRegionChange(region)
   }
 
+  _getDestinationFromDistance = (initialLat, initialLng, distance, bearing) =>
+  {
+    //console.warn('distance', distance);
+    const radiusEarth = 6378.1;
+    const bearingRad = GeolocationHelper._toRad(bearing);
+    const distanceKm = distance / 1000;
+
+    //console.warn("bearingRad",bearingRad);
+    const lat1 = GeolocationHelper._toRad(initialLat); //Current lat point converted to radians
+    const lon1 = GeolocationHelper._toRad(initialLng); //Current long point converted to radians
+
+    let lat2 = Math.asin(
+        Math.sin(lat1) * Math.cos(distanceKm / radiusEarth)
+        +
+        Math.cos(lat1) * Math.sin(distanceKm / radiusEarth) * Math.cos(bearingRad)
+    );
+
+    let lon2 = lon1
+        +
+        Math.atan2(
+            Math.sin(bearingRad) * Math.sin(distanceKm / radiusEarth) * Math.cos(lat1),
+            Math.cos(distanceKm / radiusEarth) - Math.sin(lat1) * Math.sin(lat2)
+        );
+
+    lat2 = GeolocationHelper._toDeg(lat2);
+    lon2 = GeolocationHelper._toDeg(lon2);
+
+    //console.warn(initialLat,lat2);
+    //console.warn(initialLng,lon2);
+    return {
+      latitude: lat2,
+      longitude: lon2,
+    }
+
+  };
+
   onClusterPress = (cluster) => {
 
     // cluster press behavior might be extremely custom.
     //console.log("cluster Onpress start", this.props.preserveClusterPressBehavior);
     if (!this.props.preserveClusterPressBehavior) {
-     // console.log("cluster Onpress start");
+      // console.log("cluster Onpress start");
       this.props.onClusterPress && this.props.onClusterPress(cluster.properties.cluster_id)
       return
     }
@@ -131,7 +325,7 @@ export default class ClusteredMapView extends Component {
     // get cluster children
     //console.log("cluster Onpress fitToCoordinates");
     const children = this.index.getLeaves(cluster.properties.cluster_id, this.props.clusterPressMaxChildren),
-          markers = children.map(c => c.properties.item)
+        markers = children.map(c => c.properties.item)
 
     // fit right around them, considering edge padding
     this.mapview.fitToCoordinates(markers.map(m => m.location), { edgePadding: this.props.edgePadding })
@@ -141,25 +335,27 @@ export default class ClusteredMapView extends Component {
 
   render() {
     //console.log('renderMap');
+
     return (
-      <MapView
-        { ...this.props}
-        ref={this.mapRef}
-        onRegionChange={this._onRegionChange}
-        onRegionChangeComplete={this.onRegionChangeComplete}>
-        {
-          this.props.clusteringEnabled && this.state.data.map((d) => {
-            if (d.properties.point_count === 0)
-              return this.props.renderMarker(d.properties.item)
-
-            if(this.props.renderCluster)
-            {
-              //console.log('properties',d);
+        <MapView
+            { ...this.props}
+            ref={this.mapRef}
+            onRegionChange={this._onRegionChange}
+            onRegionChangeComplete={this.onRegionChangeComplete}>
+          {
+            this.props.clusteringEnabled && this.state.data.map((d) => {
+              if (d.properties.point_count === 0)
+                return this.props.renderMarker(d.properties.item)
 
 
-              const pointCount = d.properties.point_count // eslint-disable-line camelcase
-              const latitude = d.geometry.coordinates[1],
-                  longitude = d.geometry.coordinates[0]
+              if(this.props.renderCluster)
+              {
+                //console.log('properties',d);
+
+
+                const pointCount = d.properties.point_count // eslint-disable-line camelcase
+                const latitude = d.geometry.coordinates[1],
+                    longitude = d.geometry.coordinates[0]
                 const cluster = {
                   pointCount,
                   coordinate: { latitude, longitude },
@@ -173,27 +369,57 @@ export default class ClusteredMapView extends Component {
                 id: d.properties.cluster_id,
               });*/
                 return this.props.renderCluster(cluster)
-            }
+              }
 
-            return (
-              <ClusterMarker
-                {...d}
-                onPress={this.onClusterPress}
-                textStyle={this.props.textStyle}
-                scaleUpRatio={this.props.scaleUpRatio}
-                renderCluster={this.props.renderCluster}
-                key={`cluster-${d.properties.cluster_id}`}
-                containerStyle={this.props.containerStyle}
-                clusterInitialFontSize={this.props.clusterInitialFontSize}
-                clusterInitialDimension={this.props.clusterInitialDimension} />
-            )
-          })
-        }
-        {
-          !this.props.clusteringEnabled && this.props.data.map(d => this.props.renderMarker(d))
-        }
-        {this.props.children}
-      </MapView>
+              return (
+                  <ClusterMarker
+                      {...d}
+                      onPress={this.onClusterPress}
+                      textStyle={this.props.textStyle}
+                      scaleUpRatio={this.props.scaleUpRatio}
+                      renderCluster={this.props.renderCluster}
+                      key={`cluster-${d.properties.cluster_id}`}
+                      containerStyle={this.props.containerStyle}
+                      clusterInitialFontSize={this.props.clusterInitialFontSize}
+                      clusterInitialDimension={this.props.clusterInitialDimension} />
+              )
+            })
+          }
+          {
+            !this.props.clusteringEnabled && this.props.data.map(d => this.props.renderMarker(d))
+          }
+          {this.ungroupLines.length > 0
+              ? this.ungroupLines.map((group) =>
+              {
+                console.warn(group);
+                return (
+                    <MapView.Polyline
+                        coordinates={group}
+                        strokeColor={this.props.polyColor}
+                        strokeWidth={2}
+                    />
+                );
+              })
+              : null
+          }
+
+          {this.groupCenter.length > 0
+              ? this.groupCenter.map((center) =>
+              {
+                console.warn(center);
+                return (
+                    <Marker
+                        coordinate={center}>
+                      <View>
+                        <Image source={this.props.imgCenter}/>
+                      </View>
+                    </Marker>
+                );
+              })
+              : null
+          }
+          {this.props.children}
+        </MapView>
     )
   }
 }
@@ -228,6 +454,8 @@ ClusteredMapView.propTypes = {
   clusterInitialFontSize: PropTypes.number.isRequired,
   clusterPressMaxChildren: PropTypes.number.isRequired,
   clusterInitialDimension: PropTypes.number.isRequired,
+  imgCenter: PropTypes.node.isRequired,
+  polyColor: PropTypes.string.isRequired,
   // array
   data: PropTypes.array.isRequired,
   // func
